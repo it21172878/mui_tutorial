2a38e8a62c19393729c2ae61055852e1aee31ad3
it21172878 <99156192+it21172878@users.noreply.github.com>
2023-08-10 18:11:49 +0530

    Typohraphy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div>
+      <Typography variant="h2" component={'h6'} sx={{ color: 'lightseagreen' }}>
+        Dilanka Liyanagama
+      </Typography>
       <h1>MUI Tutorial</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
     </div>
   );
 }
