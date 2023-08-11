898e3e118922b1f140f4fa38313f7a6c34413467
it21172878 <99156192+it21172878@users.noreply.github.com>
2023-08-11 10:51:50 +0530

    AppBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import Form from './components/Form';
 
 function App() {
   return (
     <div>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h4">DILANKA</Typography>
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ marginLeft: 'auto' }}
+          >
+            SignIn
+          </Button>
+        </Toolbar>
+      </AppBar>
       <Form />
     </div>
   );
